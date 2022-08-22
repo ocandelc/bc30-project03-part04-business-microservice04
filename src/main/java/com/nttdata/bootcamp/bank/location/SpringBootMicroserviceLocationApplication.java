20265eca81dbaef725f0a1300ede6550be012d95
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-22 14:04:09 -0500

    commit

--- src/main/java/com/nttdata/bootcamp/bank/location/SpringBootMicroserviceLocationApplication.java
+++ src/main/java/com/nttdata/bootcamp/bank/location/SpringBootMicroserviceLocationApplication.java
@@ -1,22 +1,14 @@
 package com.nttdata.bootcamp.bank.location;
 
-import com.nttdata.bootcamp.bank.location.model.dao.inte.LocationDaoInte;
-import com.nttdata.bootcamp.bank.location.model.document.Location;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
-
-import java.util.Date;
 
 @SpringBootApplication
 public class SpringBootMicroserviceLocationApplication implements CommandLineRunner {
 
-    @Autowired
-    LocationDaoInte dao;
     private static final Logger log = LoggerFactory.getLogger(SpringBootMicroserviceLocationApplication.class);
 
     public static void main(String[] args) {
